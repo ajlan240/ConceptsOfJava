e34e26b5e62c08d6cac54d0bd5618b5f1b406d00
ajlan240 <144353361+ajlan240@users.noreply.github.com>
2024-09-29 13:45:32 +0530

    Update 052-Map.java

--- 052-Map.java
+++ 052-Map.java
@@ -9,12 +9,12 @@ class Lesson52 {
         Map<String, Integer> students  = new HashMap<>();
         students.put("Ajlan", 34);
         students.put("Moly", 94);
-        students.put("Deepu", 73);
+        students.put("Rahul", 73);
         students.put("Manu", 64);
         // Values Can be changed
         students.put("Ajlan", 84);
         System.out.println(students);
-        System.out.println(students.get("Rayyan"));
+        System.out.println(students.get("Moly"));
 
         // to print all the keys 
         System.out.println(students.keySet());
